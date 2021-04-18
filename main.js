dfb6612b53f358fecc492743d63fc2f59456aa13
skilloo-coder <82319063+skilloo-coder@users.noreply.github.com>
2021-04-18 17:15:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 console.log('HELLO this is Ayush _how are you')
 
-alert('Hi ! I am a hacker . I have started to hack you just wait a minute . Ha! Ha! Ha!.If you want to save you click on Delhi')
+alert(' HELLO ! This is Ayush __HOW ARE YOU ')
